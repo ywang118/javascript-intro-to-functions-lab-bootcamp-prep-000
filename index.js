e66aa7ae7c8728b98b609fa4f841ab25a20e0174
wangfanqoo@126.com Wang <wangfanqoo@126.com>
2018-03-20 19:55:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function sayHiToGrandma(string){
   if (string.toLowerCase() === string) {
     return "I can't hear you!"
   } else if (string.toUpperCase() === string){
-    return ""
+    return "YES INDEED!"
   }
   
 }
