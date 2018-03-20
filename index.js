355fd17f5791d99500b7d12aa232b0360b1fb77f
wangfanqoo@126.com Wang <wangfanqoo@126.com>
2018-03-20 19:49:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function logWhisper(string) {
 
 
 function sayHiToGrandma(string){
-  if (string === string.toLowerCase()) {
+  if (string.toLowerCase() === string) {
     console.log ( "I can\'t hear you!")
   }
 }
